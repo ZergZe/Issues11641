2cfda8465963633baab86d717fd4bcc5fcd7c5ec
ZergZe <3338813+ZergZe@users.noreply.github.com>
2019-08-07 12:56:28 +0300

    Add project

---
+++ Pages/Counter2.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+
+namespace SomeNameSpace
+{
+    public partial class Counter2Base : ComponentBase, IDisposable
+    {
+        public ElementRef input;
+        public string Text;
+
+        [Inject] IJSRuntime JsRuntime { get; set; }
+        [Inject] IComponentContext ComponentContext { get; set; }
+
+        object objReg;
+        object ObjReg {
+            get {
+                if (objReg == null)
+                    objReg = DotNetObjectRef.Create(this);
+                return objReg;
+            }
+        }
+
+        protected override void OnAfterRender()
+        {
+            SubscribeEvents();
+        }
+
+        public async void SubscribeEvents()
+        {
+            JSRuntime.SetCurrentJSRuntime(JsRuntime);
+            await JsRuntime.InvokeAsync<object>(
+                    "exampleJsFunctions.SubscribeEvents", input, ObjReg);
+        }
+
+        [JSInvokable]
+        public void LostFocus(string text)
+        {
+            Text = text;
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            var disposableObjReg = objReg as IDisposable;
+            if(disposableObjReg != null)
+                disposableObjReg.Dispose();
+        }
+    }
+}
---
+++ wwwroot/exampleJsInterop.js
@@ -0,0 +1,9 @@
+﻿window.exampleJsFunctions = {
+    SubscribeEvents: function (element, dotnetHelper) {
+        element.addEventListener('blur', function (e) {
+            window.setTimeout(function () {
+                dotnetHelper.invokeMethodAsync('LostFocus', e.target.value);
+            }, 3000);
+        });
+    },
+};
